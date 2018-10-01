077dbf77103def243f56dec1eea57779ff323d84
OnurSevket <onursevket44@gmail.com>
2018-10-01 15:10:34 +0300

    İnitial Commit

---
+++ First_Time.py
@@ -0,0 +1,141 @@
+#print("Hello World.Its Run")
+
+# region Exercise 1
+
+# x = 1
+# if x == 1:
+#     print("x is 1")
+
+# endregion
+
+# region Exercise 2
+
+# myint=8
+# print(myint)
+
+# endregion
+
+# region Exercise 3
+
+# myfloat=0.7
+# print(myfloat)
+# myfloat=float(7)
+# print(myfloat)
+
+# endregion
+
+# region Exercise 4
+
+# mystring='hello'
+# print(mystring)
+# mystring="Hello World"
+# print(mystring)
+
+
+# endregion
+
+# region Exercise 5
+
+# mystring="don't worry "
+# print(mystring)
+
+# endregion
+
+# region Exercise 6
+
+# firstNumber = 5
+# secondNumber = 6
+# SumOfNumbers = firstNumber+secondNumber
+# print(SumOfNumbers)
+
+# hello = "Hello"
+# world = "World"
+# helloWorld = hello + ' '+world
+# print(helloWorld)
+
+
+# endregion
+
+# region Exercise 7
+
+# a, b = 3, 4
+# print(a, b)
+
+# endregion
+
+# region Exercise 8
+
+# mystring = "hello"
+# myfloat = float(10.0)
+# myint = 5
+
+# if(mystring == "hello"):
+#     print("String: %s" % mystring)
+# if isinstance(myfloat, float) and myfloat == 10.0:
+#     print("Float: %f" % myfloat)
+# if isinstance(myint,int) and myint==5:
+#     print("Integer: %d" % myint)
+
+
+# endregion
+
+# region Exercise 9
+
+# mylist = []
+# mylist.append(1)
+# mylist.append(2)
+# mylist.append(3)
+# print(mylist[0])
+# print(mylist[1])
+# print(mylist[2])
+
+# for x in mylist:
+#     print(x, end=" ")
+
+# endregion
+
+
+# region Exercise 10
+
+# numbers = [1, 2, 3]
+# names=["hello","world"]
+
+# print(numbers)
+# print(names)
+
+# endregion
+
+
+# region Exercise 11
+
+# number = 1+2*3/4.0
+# print(number)
+
+# endregion
+
+
+# region Exercise 12
+
+# reminder = 11 % 3
+# print(reminder)
+
+# squared = 7**2
+# cubed = 2**3
+# print(squared)
+# print(cubed)
+
+# lotsofHello = " hello"*10
+# print(lotsofHello)
+
+even_numbers = [2, 4, 6, 8]
+odd_numbers = [1, 3, 5, 7]
+all_numbers = odd_numbers+even_numbers
+print(all_numbers)
+
+# endregion
+
+
+# region Exercise
+
+
+# endregion

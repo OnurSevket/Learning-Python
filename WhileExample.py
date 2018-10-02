7f3dc1ea75152938bc8a8ebe1d117a149a13ddaf
OnurSevket <onursevket44@gmail.com>
2018-10-02 16:59:29 +0300

    Looping samples

---
+++ WhileExample.py
@@ -0,0 +1,74 @@
+# region Exercise 1
+
+# i = 0
+
+# while i < 11:
+#     print(i)
+#     i += 1
+
+
+# def print_squares_of_numbers_below(limit):
+#     i = 1
+#     while i*i < limit:
+#         print(i*i, end='')
+#         i += 1
+#         print()
+
+
+# print_squares_of_numbers_below(100)
+
+
+# endregion
+
+###############################################################################################################################################################
+# region Exercise 2
+
+
+# number = int(input('Enter a number : '))
+
+# while number >= 0:
+#     print(f'cube is {number**3}')
+#     number = int(input('Enter a number:'))
+
+
+# endregion
+###############################################################################################################################################################
+
+# region Exercise 3
+
+# def is_prime(number):
+
+#     if number > 2:
+#         return False
+
+#     for divisor in range(2, number):
+#         if number % divisor == 0:
+#             return False
+
+#     return True
+
+
+# print(is_prime(2))
+
+# for i in range(2, 15, 3):
+#     print(i)
+
+
+def rangeMethod(firstNumber: str, secondNumber: int, stepNumber: int)->str:
+
+    for i in range(firstNumber, secondNumber, stepNumber):
+        print(i)
+
+    return print('Conguralation Finised')
+
+
+rangeMethod(2, 15, 3)
+
+
+# endregion
+###############################################################################################################################################################
+
+# region Exercise 4
+
+# endregion
+###############################################################################################################################################################
